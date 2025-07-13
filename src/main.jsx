ab5aa7ff2c2bbdc2b89c5d8a4e0bb8cdcc7b80da
Jiiker <motive46@naver.com>
2025-07-13 21:33:32 +0900

    feat: sentry 설정 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
-import 'mapbox-gl/dist/mapbox-gl.css';
-import './index.css';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import "mapbox-gl/dist/mapbox-gl.css";
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
 
-import App from './App.jsx'
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 1.0,
+  allowUrls: ["localhost", /^https:\/\/project-g-seven\.vercel\.app/],
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Sentry.ErrorBoundary fallback={<p>Something went wrong</p>}>
+      <App />
+    </Sentry.ErrorBoundary>
+  </StrictMode>
+);
